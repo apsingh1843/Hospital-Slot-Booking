c8873a1c1d3b9a1710a130924149131ae4eeeacf
Ayush Pratap Singh <apsingh1843@gmail.com>
2021-07-31 00:24:58 +0530

    deployment fix

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -7,7 +7,8 @@ const middleware = [thunk];
 
 const store = createStore(RootReducer, initialState, compose(
   applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
 export default store;
+
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
